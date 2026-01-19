a75de104507ec30fb99f85de989f4018d8172cc9
David Jenniskens <david_jenniskens@hotmail.com>
2026-01-19 12:26:58 +0100

    "Hamburger"menu toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 console.log("hi");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+.hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
