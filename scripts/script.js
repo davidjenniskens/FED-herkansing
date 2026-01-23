8015f7ee687a99364dd3b4f69d8146cf3021511b
David Jenniskens <david_jenniskens@hotmail.com>
2026-01-23 17:27:36 +0100

    script toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,16 @@ console.log("hi");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-.hamburger.addEventListener("click", () =>{
+hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
+
+const buttons = document.querySelectorAll('.js-toggle');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    buttons.forEach(b => b.classList.remove('active')); // alles uit [web:5]
+    btn.classList.add('active'); // geklikte aan [web:5]
+  });
+});
